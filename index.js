9312b973f2b7892c0fa9b0871dc431ceefe5d044
vulam.17 <vulam.17@gmail.com>
2022-10-09 21:10:20 +0700

    4nd

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let interval = setInterval(getTime,1000)
 let date, hour, min, sec, soundAllow = false, newHour = 0
-let hourOffset = 0, minOffset = 0, secOffset = 0, r = 0, c = 0
+let hourOffset = 0, minOffset = 0, secOffset = 0, r = 0, c = 0, dotHourCX = 0, dotHourCY = 0, dotHourAlpha = 0
+dotMinCX = 0, dotMinCY = 0, dotMinAlpha = 0,dotSecCX = 0, dotSecCY = 0, dotSecAlpha = 0
 function getTime()
 {
     r = window.innerWidth * 0.95 * 0.33 * 0.4
     document.getElementById("cont").style.height = window.innerWidth * 0.333 + "px"
+    //console.log(Math.sin(Math.PI/2))
+    
     if(window.innerWidth < 280) 
     {
         document.getElementById("cont").style.height = window.innerWidth * 3 + "px"
@@ -27,6 +30,25 @@ function getTime()
     hour = date.getHours()
     min = date.getMinutes()
     sec = date.getSeconds()
+    dotHourAlpha = (2 * Math.PI/24) * hour
+    dotHourCX = window.innerWidth / 6 + Math.cos(dotHourAlpha) * r
+    dotHourCY = window.innerWidth / 6 + Math.sin(dotHourAlpha) * r
+    document.getElementById("dothour").setAttribute("cx",dotHourCX)
+    document.getElementById("dothour").setAttribute("cy",dotHourCY)
+
+    dotMinAlpha = (2 * Math.PI/60) * min
+    dotMinCX = window.innerWidth / 6 + Math.cos(dotMinAlpha) * r
+    dotMinCY = window.innerWidth / 6 + Math.sin(dotMinAlpha) * r
+    document.getElementById("dotmin").setAttribute("cx",dotMinCX)
+    document.getElementById("dotmin").setAttribute("cy",dotMinCY)
+
+    dotSecAlpha = (2 * Math.PI/60) * sec
+    dotSecCX = window.innerWidth / 6 + Math.cos(dotSecAlpha) * r
+    dotSecCY = window.innerWidth / 6 + Math.sin(dotSecAlpha) * r
+    document.getElementById("dotsec").setAttribute("cx",dotSecCX)
+    document.getElementById("dotsec").setAttribute("cy",dotSecCY)
+
+
     hourOffset = c - (hour * (c/24))
     minOffset = c - (min * (c/60))
     secOffset = c - (sec * (c/60))
@@ -35,11 +57,9 @@ function getTime()
         {
             document.getElementById("music2").pause()
             document.getElementById("music1").play() 
-            console.log("+")
         }
         if((sec < 6) && (min == 00) && (newHour < 1))
         {
-            console.log("here")
             document.getElementById("music1").pause()
             document.getElementById("music2").play() 
             newHour++
